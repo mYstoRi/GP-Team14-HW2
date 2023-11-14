2fa9fcdb454bfd25cd376bd01341214ef15abaca
Aru Liao <gary1148@gmail.com>
2023-11-14 23:21:17 +0800

    Add timer and reloading scene when the player die.

--- team14_HW2/Assets/Scripts/Player/PlayerEntity.cs
+++ team14_HW2/Assets/Scripts/Player/PlayerEntity.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerEntity : EntityGeneric
 {
+    public float SurviveTimer
+    {
+        get
+        {
+            return surviveTimer;
+        }
+        set
+        {
+            UpdateTimer(value);
+        }
+    }
+
+    #region VARIABLES
+    [SerializeField] float surviveTime = 100;   //survive time in seconds, defult: 1 min 40 sec
+    float surviveTimer = 0;
+    private Animator anim;
+    #endregion
+
+    #region METHODS
     public override void UpdateHealth(float newValue)
     {
         base.UpdateHealth(newValue);
         UIManager.instance.UpdateHpUI(Health, MaxHealth);
+    }
+    public void UpdateTimer(float newValue)
+    {
+        surviveTimer = newValue;
+        UIManager.instance.UpdateTimerUI(newValue);
     }
 
     IEnumerator Dying()
     {
         yield return new WaitForSeconds(Time.deltaTime);
         anim.SetBool("dead", false);
+        LevelManager.instance.ReloadCurrentLevel();
     }
 
     public override void Die()
@@ -24,19 +50,18 @@ public class PlayerEntity : EntityGeneric
         StartCoroutine(Dying());
     }
 
-    // Test for taking damage , will be deleted later
-    [SerializeField] bool enableTestingHp = false;
-    float test = 1;
-    private Animator anim;
-
     private void Start()
     {
         anim = GetComponent<Animator>();
         IsDied = false;
+        SurviveTimer = surviveTime;
     }
 
     private void Update() 
     {   
+        SurviveTimer -= Time.deltaTime;
+
+        #region TEST
         if(!enableTestingHp) return;
         if(test <= 0)
         {
@@ -44,5 +69,12 @@ public class PlayerEntity : EntityGeneric
             test = 1;
         }
         test -= Time.deltaTime;
+        #endregion
     }
+    #endregion
+
+    // Test for taking damage , will be deleted later
+    float test = 1;
+    [SerializeField] bool enableTestingHp = false;
+
 }
